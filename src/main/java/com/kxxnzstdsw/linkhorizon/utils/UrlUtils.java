5f823c19c6e501845dd31087e482780f63fd6cb9
快醒醒你只是条单身汪 <langbiantianya@outlook.com>
2024-12-03 14:41:51 +0800

    feat: init app

--- src/main/java/com/kxxnzstdsw/linkhorizon/utils/UrlUtils.java
+++ src/main/java/com/kxxnzstdsw/linkhorizon/utils/UrlUtils.java
@@ -3,19 +3,19 @@ package com.kxxnzstdsw.linkhorizon.utils;
 import org.apache.logging.log4j.util.Strings;
 
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class UrlUtils {
-    @SuppressWarnings("deprecation")
     public static boolean validURL(String urlString) {
         if (Strings.isEmpty(urlString)) {
             return false;
         }
 
         try {
-            new URL(urlString);
+            new URI(urlString).toURL();
             return true;
-        } catch (MalformedURLException e) {
+        } catch (MalformedURLException | URISyntaxException e) {
             return false;
         }
     }
